e6a46dc7bf85fca45b2d19cca885ad4561dee714
Alaaeldeen Hamed <mregydev@gmail.com>
2026-08-17 20:57:06 +0300

    chore: update mobile download barcode in web

--- constants/mobileApp.ts
+++ constants/mobileApp.ts
@@ -2,6 +2,6 @@ export const ANDROID_APP_PACKAGE = "com.threelagi.mobile";
 
 export const ANDROID_APP_URL =
   process.env.EXPO_PUBLIC_ANDROID_APP_URL ??
-  "https://u.expo.dev/update/7677864b-6101-4470-9f05-677568169623";
+  "https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40abdallah_medhat%2Fpoints-app-fb3663b6a90847248e58f3316982626e-signed.apk";
 
 export const ANDROID_APP_QR = require("@/assets/images/android-app-qr.png");
